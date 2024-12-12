cb3090a0ed92dda5be15fd7a77a10433b98809ee
Abdussamad Syed <abdussamad.mi5@gmail.com>
2024-12-12 14:54:11 -0500

    Adding exception handlers.

---
+++ application/auth/src/main/java/com/example/auth/dto/ErrorResponse.java
@@ -0,0 +1,5 @@
+package com.example.auth.dto;
+
+public class ErrorResponse extends BaseResponse {
+
+}
---
+++ application/auth/src/main/java/com/example/auth/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.example.auth.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.example.auth.dto.ErrorResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleError(IllegalArgumentException exception) {
+        ErrorResponse response = new ErrorResponse();
+        response.setMessage(exception.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGenericException(Exception ex) {
+        ErrorResponse response = new ErrorResponse();
+        response.setMessage("An unexpected error occurred");
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
