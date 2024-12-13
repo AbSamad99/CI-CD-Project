45e879e7d3cbf3500c00b4142e0be5f8c2f42342
Abdussamad Syed <abdussamad.mi5@gmail.com>
2024-12-13 15:58:53 -0500

    Disabling spring security autoconfiguration for now.

--- application/auth/src/main/java/com/example/auth/AuthApplication.java
+++ application/auth/src/main/java/com/example/auth/AuthApplication.java
@@ -3,7 +3,9 @@ package com.example.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// Disabling spring security (for now)
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class })
 public class AuthApplication {
 
 	public static void main(String[] args) {
